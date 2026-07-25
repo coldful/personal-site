a59f9efe70412549d9419d6f5ab2cf5fc0f681c6
Serggg78 <serge300978@gmail.com>
2026-07-25 17:50:13 +0300

    Design improuvement

--- src/main.js
+++ src/main.js
@@ -85,10 +85,6 @@ async function initBlog() {
 
 function bootstrap() {
   applyMeta(siteData.meta);
-  document.documentElement.style.setProperty(
-    '--nature-image',
-    `url('${import.meta.env.BASE_URL}nature.jpg')`,
-  );
   document.getElementById('app').innerHTML = createPageMarkup(siteData);
   initMobileMenu();
   initSmoothScroll();
